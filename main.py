170680729d9d413082cf28fdb2acb7028c62678b
TaiGambol <TaiJParry@googlemail.com>
2025-08-24 02:09:44 +0100

    First solving steps - searching a specified row/column/block for a naked single of a specified digit

--- main.py
+++ main.py
@@ -14,10 +14,17 @@ def read_sudoku(sudoku_path):
         for j, num in enumerate(row):
             if num != ".":
                 grid.enter_number(num, k, j)
-    grid.print_grid()
-    print(grid.possibilities.grid[0][7])
+    return grid
+
+def solve_step(grid):
+    pass
 
 def main():
-    read_sudoku("./puzzles/puzzle.txt")
+    grid = read_sudoku("./puzzles/puzzle.txt")
+    grid.print_grid()
+    print(grid.possibilities.grid[0][7])
+    print(grid.possibilities.is_possible("2", 0, 7))
+    grid.naked_singles_col(8, 5)
+    grid.print_grid()
 
 main()
--- sudoku_classes.py
+++ sudoku_classes.py
@@ -1,3 +1,5 @@
+import math
+
 class SudokuGrid:
     def __init__(self):
         self.grid = []
@@ -14,10 +16,14 @@ class SudokuGrid:
         print(f"Confirming correct entering: {self.grid[row][col]} now equals {num}.")
         print(f"Beginning to remove {num} from possibles in this row and column.")
         for i in range(9):
-            print(f"Removing from row {row}, column {i}")
+            #print(f"Removing from row {row}, column {i}")
             self.possibilities.remove_possible(num, row, i)
-            print(f"Removing from row {i}, column {col}")
+        for i in range(9):
+            #print(f"Removing from row {i}, column {col}")
             self.possibilities.remove_possible(num, i, col)
+        
+        print(f"Rows and columns checked, removing from block...")
+        self.possibilities.remove_possible_in_block(num, row, col)
         print(f"Possiblities should be removed now. Testing...")
         print(self.possibilities.grid[5][col])
         print(f"The above list shouldn't have the number {num} in it.")
@@ -25,6 +31,81 @@ class SudokuGrid:
     def print_grid(self):
         for row in self.grid:
             print(row)
+
+    def naked_singles_row(self, num, row):
+        print(f"Checking for naked singles in the area of row {row}")
+        num = str(num)
+        single = False
+        single_row = row
+        single_col = 0
+        # Checking each cell in the row
+        for j in range(9):
+            if self.possibilities.grid[row][j] == num and single is not True:
+                print(f"Found a possible match, and no others yet.")
+                single = True
+                single_col = j
+            elif self.possibilities.grid[row][j] == num and single is True:
+                print(f"Found a second possible match, no naked singles here.")
+                single = False
+                single_col = 0
+                break
+        if single is True:
+            self.enter_number(num, single_row, single_col)
+
+
+    def naked_singles_col(self, num, col):
+        print(f"Checking for naked singles in the area of column {col}")
+        num = str(num)
+        single = False
+        single_row = 0
+        single_col = col
+        # Checking each cell in the row
+        for j in range(9):
+            print(f"Checking cell {j} in column {col}")
+            if num in self.possibilities.grid[j][col] and single is not True:
+                print(f"Found a possible match, and no others yet.")
+                single = True
+                single_row = j
+            elif num in self.possibilities.grid[j][col] and single is True:
+                print(f"Found a second possible match, no naked singles here.")
+                single = False
+                single_row = 0
+                break
+        if single is True:
+            self.enter_number(num, single_row, single_col)
+
+
+
+    def naked_singles_block(self, num, row, col):
+        print(f"Checking for naked singles in the area of row {row}, col {col}")
+        num = str(num)
+        single = False
+        single_row = 0
+        single_col = 0
+        g = row + 1
+        h = col + 1
+        g2 = math.ceil(g/3)
+        h2 = math.ceil(h/3)
+        g = g2 * 3
+        h = h2 * 3
+        g2 = g-3
+        h2 = h-3
+        for i in range(g2, g):
+            for j in range(h2, h):
+                print(f"Checking {i},{j} on the grid")
+                if self.possibilities.grid[i][j] == num and single is not True:
+                    print(f"Found a possible match, and no others yet.")
+                    single = True
+                    single_row = i
+                    single_col = j
+                elif self.possibilities.grid[i][j] == num and single is True:
+                    print(f"Found a second possible match, no naked singles here.")
+                    single = False
+                    single_row = 0
+                    single_col = 0
+                    break
+        if single is True:
+            self.enter_number(num, single_row, single_col)
 
 
 class Possibilities(SudokuGrid):
@@ -36,10 +117,32 @@ class Possibilities(SudokuGrid):
                 list.append(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
             self.grid.append(list)
     
+    
     def remove_possible(self, num, row, col):
         if num in self.grid[row][col]:
             print(f"Found {num} in possibles here, removing it.")
             self.grid[row][col].remove(num)
     
+    def remove_possible_in_block(self, num, row, col):
+        #print(f"Running initial math to work out what block we're in.")
+        g = row + 1
+        h = col + 1
+        #print(f"g set to {g}, h set to {h}")
+        g2 = math.ceil(g/3)
+        h2 = math.ceil(h/3)
+        #print(f"{g}/3 ceiling'd = {g2}, {h}/3 ceiling'd = {h2}")
+        g = g2 * 3
+        h = h2 * 3
+        #print(f"{g2}x3 = {g}, {h2}x3 = {h}")
+        g2 = g-3
+        h2 = h-3
+        #print(f"Start points set: row search starting at {g2}, column search starting at {h2}")
+        #print(f"Searching for the proper box now.")
+        for i in range(g2, g):
+            #print(f"We're in row {i}")
+            for j in range(h2, h):
+                #print(f"We're in column {j}")
+                self.remove_possible(num, i, j)
+        
     def is_possible(self, num, row, col):
         return num in self.grid[row][col]
